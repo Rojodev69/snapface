8566ac75564988f20c3ff2501d3f2b90c24a51f6
RojoDev <rojo.valisoa.dev@gmail.com>
2022-10-02 12:19:32 +0300

    data/event Binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  imageUrl!: string; 
+  titleButton!: string;
+  issnaped!: boolean;
 
-
-
+  ngOnInit() {
+    this.title = 'Rojo';
+    this.description = 'Mon pdp';
+    this.createDate = new Date();
+    this.snaps = 0;  
+    this.imageUrl = 'https://www.01net.com/app/uploads/2020/12/MEA-Photos-App-windows-10-1.jpg';
+    this.issnaped = false;
+    this.titleButton = 'oh snaped'
+  }
+  onClickSnaps(){
+    if (this.issnaped == false){
+      this.snaps ++;
+      this.titleButton =  'oups snaped';
+      this.issnaped = true;
+    }
+    else {
+      this.issnaped = false;
+      this.snaps --;
+      this.titleButton =  'oh snaped';
+    }
+  }
 }
